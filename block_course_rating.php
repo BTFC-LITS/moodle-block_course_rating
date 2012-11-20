1aed89baba325437774b3aadb7057c69ba24e8d9
Tony Butler <tonyjbutler@gmail.com>
2012-11-20 17:04:41 +0000

    Initial commit

---
+++ block_course_rating.php
@@ -0,0 +1,106 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Block for displaying course rating.
+ *
+ * @package   block_course_rating
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class block_course_rating extends block_base {
+
+    public function init() {
+        $this->title = get_string('pluginname', 'block_course_rating');
+    }
+
+    /**
+     * Which page types this block may appear on
+     * @return array
+     */
+    public function applicable_formats() {
+        return array('course-view' => true);
+    }
+
+    public function get_content() {
+        global $CFG, $DB, $COURSE, $OUTPUT;
+
+        if ($this->content !== NULL) {
+            return $this->content;
+        }
+
+        $courseid = $COURSE->id;
+        $viewrating = '';
+        $link = '';
+
+        // Select rating for course
+        $rating = $DB->get_field('block_course_rating', 'rating', array('courseid'=>$courseid));
+
+        // Get context so we can check capabilities.
+        $blockcontext = get_context_instance(CONTEXT_COURSE, $courseid);
+
+        if (has_capability('block/course_rating:viewrating', $blockcontext)) {
+            $viewrating = true;
+        }
+        else $viewrating = false;
+
+        // Reassure teachers that students can't see their course rating
+        $note = '<div style="font-size: 0.85em; text-align: center;">' .
+                get_string('notvisible', 'block_course_rating') . '</div>';
+
+        // Display rating for course and link to course ratings explanation
+        if ($viewrating == true and $rating == 'Gold' ) {
+            $img = '<div style="margin: 6px; text-align:center;"><img src="' .
+                    $CFG->wwwroot . '/blocks/course_rating/pix/gold.png"
+                    alt="Gold" title="Gold" width="90" height="98" /></div>';
+            $link = '<div align="center">' . $OUTPUT->help_icon('ratings_explained',
+                    'block_course_rating', get_string('meaning', 'block_course_rating')) .
+                    '</div>';
+        } else if ($viewrating == true and $rating == 'Silver' ) {
+            $img = '<div style="margin: 6px; text-align:center;"><img src="' .
+                    $CFG->wwwroot . '/blocks/course_rating/pix/silver.png"
+                    alt="Silver" title="Silver" width="90" height="98" /></div>';
+            $link = '<div align="center">' . $OUTPUT->help_icon('ratings_explained',
+                    'block_course_rating', get_string('improve', 'block_course_rating')) .
+                    '</div>';
+        } else if ($viewrating == true and $rating == 'Bronze' ) {
+            $img = '<div style="margin: 6px; text-align:center;"><img src="' .
+                    $CFG->wwwroot . '/blocks/course_rating/pix/bronze.png"
+                    alt="Bronze" title="Bronze" width="90" height="98" /></div>';
+            $link = '<div align="center">' . $OUTPUT->help_icon('ratings_explained',
+                    'block_course_rating', get_string('improve', 'block_course_rating')) .
+                    '</div>';
+        } else if ($viewrating == true and $rating == '' ) {
+            $img = '<div style="margin: 6px; text-align:center;"><img src="' .
+                    $CFG->wwwroot . '/blocks/course_rating/pix/in_development.png"
+                    alt="In development" title="In development" width="90" height="90" /></div>';
+            $link = '<div align="center">' . $OUTPUT->help_icon('ratings_explained',
+                    'block_course_rating', get_string('improve', 'block_course_rating')) .
+                    '</div>';
+        } else {
+            $img = '';
+            $viewrating = '';
+            $link = '';
+        }
+        $this->content         = new stdClass;
+        $this->content->text   = $note . $img . $link;
+        return $this->content;
+        return $this->content->text;
+
+    }
+}
---
+++ lang/en/block_course_rating.php
@@ -0,0 +1,83 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_course_rating', language 'en', branch 'MOODLE_22_STABLE'
+ *
+ * @package   block_course_rating
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['course_rating:viewrating'] = 'View the course rating';
+$string['improve'] = 'How can I improve my course rating?';
+$string['meaning'] = 'What does this mean?';
+$string['notvisible'] = 'This block is not visible to students';
+$string['pluginname'] = 'Course rating';
+$string['ratings_explained'] = 'Ratings explained';
+$string['ratings_explained_help'] = '<h1>What are course ratings?</h1>
+<p>The course ratings system is an initiative to improve the quality of courses on Moodle. This will
+    improve the experience of the learner and lead to better success rates and student retention. Each
+    course is graded with a medal: Gold, Silver, Bronze or In Development.</p>
+<h2>What do these grades mean?</h2>
+<table cellpadding="6">
+    <tr>
+        <td><img title="Bronze" width="90" height="98" alt="Bronze" src="/blocks/course_rating/pix/bronze.png" /></td>
+        <td>A <strong>Bronze</strong> course is regularly accessed by at least half the enrolled students
+            and contains a broad selection of well-used resources.</td>
+    </tr>
+    <tr>
+        <td><img title="Silver" width="90" height="98" alt="Silver" src="/blocks/course_rating/pix/silver.png" /></td>
+        <td>A <strong>Silver</strong> course includes all of the bronze course elements but in addition
+            contains a selection of interactive material to engage the learner.</td>
+    </tr>
+    <tr>
+        <td><img title="Gold" width="90" height="98" alt="Gold" src="/blocks/course_rating/pix/gold.png" /></td>
+        <td>A <strong>Gold</strong> course contains all elements of a bronze and silver course but in
+            addition contains activities which encourage the learner to collaborate with their fellow
+            course mates and the teacher. In this way the learner contributes to the content of the course.</td>
+    </tr>
+    <tr>
+        <td><img title="In Development" width="90" height="90" alt="In Development" src="/blocks/course_rating/pix/in_development.png" /></td>
+        <td>A course deemed to be <strong>In Development</strong> falls short of a bronze course.</td>
+    </tr>
+</table>
+<h2>How can I improve my course rating?</h2>
+<p>The easiest way to improve your course rating and better support your students, is to contact Joanna
+    in the Learning and Teaching Centre at Bispham Campus. She can advise you on good practice
+    in e-Learning and direct you to appropriate online and face-to-face training sessions.</p>
+<div style="background-color: #EEE; padding: 12px;">
+    <h3 style="margin-top: 0;">You can contact the Learning and Teaching Centre using this information</h3>
+    <table>
+        <tr style="vertical-align: top">
+            <td><strong>Who:&nbsp;</strong></td>
+            <td>Joanna Butler (eLearning Facilitator)</td>
+        </tr>
+        <tr style="vertical-align: top">
+            <td><strong>Where:&nbsp;</strong></td>
+            <td>Learning and Teaching Centre (Second floor of Garstang Building on Bispham Campus)</td>
+        </tr>
+        <tr style="vertical-align: top">
+            <td><strong>Tel:&nbsp;</strong></td>
+            <td>01253 504287 or ext 4287</td>
+        </tr>
+        <tr style="vertical-align: top">
+            <td><strong>Email:&nbsp;</strong></td>
+            <td><a href="mailto:ltc@blackpool.ac.uk">ltc@blackpool.ac.uk</a></td>
+        </tr>
+    </table>
+</div>';
